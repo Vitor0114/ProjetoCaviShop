46faa0676c2de60b930fcd8ff2530b4c76bf6994
Vitor0114 <vitorgustav0114@gmail.com>
2023-08-18 03:50:37 -0300

    alteracoes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
   const campoBusca = document.getElementById('productSearch');
   const produtos = document.querySelectorAll('.product');
+  const carrinho = [];
 
   campoBusca.addEventListener('input', function () {
-    const termoBusca = campoBusca.value.trim().toLowerCase();
+      const termoBusca = campoBusca.value.trim().toLowerCase();
 
-    produtos.forEach(produto => {
-      const nomeProduto = produto.querySelector('h2').textContent.toLowerCase();
+      produtos.forEach(produto => {
+          const nomeProduto = produto.querySelector('h2').textContent.toLowerCase();
 
-      if (nomeProduto.includes(termoBusca)) {
-        produto.style.display = 'block';
-      } else {
-        produto.style.display = 'none';
-      }
-    });
+          if (nomeProduto.includes(termoBusca)) {
+              produto.style.display = 'block';
+          } else {
+              produto.style.display = 'none';
+          }
+      });
   });
+
+  function adicionarAoCarrinho(nomeProduto, preco) {
+      carrinho.push({ nome: nomeProduto, preco: preco });
+      alert('Produto adicionado ao carrinho: ' + nomeProduto);
+  }
 });
